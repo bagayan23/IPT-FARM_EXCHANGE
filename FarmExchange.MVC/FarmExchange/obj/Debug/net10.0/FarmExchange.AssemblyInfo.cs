c055c10e6c6bf3dc695f9aaa85800e9002c08d04
bagayan23 <59055255+bagayan23@users.noreply.github.com>
2025-12-01 04:16:10 +0800

    updates

--- FarmExchange.MVC/FarmExchange/obj/Debug/net10.0/FarmExchange.AssemblyInfo.cs
+++ FarmExchange.MVC/FarmExchange/obj/Debug/net10.0/FarmExchange.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FarmExchange")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db3bda3f7cbbb4607fb318ae79a12c1b86a6a08d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+971d221858b338d223ba5a3bfe3c6e6b0c2622d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("FarmExchange")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FarmExchange")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
